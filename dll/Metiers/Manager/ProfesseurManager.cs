dd0bd64c1dee66ea2197365bd9090ac818f144d5
Mihaly Varga <mihaly.varga@etu.unice.fr>
2019-02-05 16:10:36 +0100

    Ajouter 2 classes dans dll. Professeur, et Professeur Manager

---
+++ dll/Metiers/Manager/ProfesseurManager.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dll.Metiers.Manager
+{
+   internal static class ProfesseurManager
+    {
+        internal static Professeur Load(int id)
+        {
+            Professeur professeur = new Professeur();
+            string connStr = Utils.GetConn;
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                string sql = "SELECT * FROM Professeur WHERE id = @id";
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.Parameters.Add(new MySqlParameter("@id", id));
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            fill(professeur, dr);
+                        }
+                    }
+                }
+            }
+
+            return professeur;
+        }
+
+        internal static void fill(Professeur item, MySqlDataReader dr)
+        {
+            item.Id = (int)dr["id"];
+            item.Nom = (string)dr["nom"];
+            item.Prenom = (string)dr["prenom"];
+        }
+    }
+}
