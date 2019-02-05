9458ef6beb6efd3fa85278b9b8509eb27aaf88f9
Mihaly Varga <mihaly.varga@etu.unice.fr>
2019-02-05 16:55:44 +0100

    Lister les utilisateur par classe

--- dll/Metiers/Manager/UtilisateurManager.cs
+++ dll/Metiers/Manager/UtilisateurManager.cs
@@ -33,11 +33,39 @@ namespace dll.Metiers.Manager
             return utilisateur;
         }
 
+
+        internal static List<Utilisateur> ListClasse(int numero)
+        {
+            List<Utilisateur> laClasse = new List<Utilisateur>();
+            string connStr = Utils.GetConn;
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                string sql = "SELECT * FROM Utilisateur WHERE u_classe = @numero";
+                conn.Open();
+                using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.Parameters.Add(new MySqlParameter("@numero", numero));
+                    using (MySqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            Utilisateur unEleve = new Utilisateur();
+                            fill(unEleve, dr);
+                            laClasse.Add(unEleve);
+                        }
+                    }
+                }
+
+            }
+            return laClasse;
+        }
+
         internal static void fill(Utilisateur item, MySqlDataReader dr)
         {
             item.Id = (int)dr["u_id"];
             item.Niveau = (NiveauIds)dr["u_niveau"];
             item.Nom = (string)dr["u_nom"];
+            item.Prenom = (string)dr["u_prenom"];
         }
     }
 }
--- dll/Metiers/Utilisateur.cs
+++ dll/Metiers/Utilisateur.cs
@@ -13,15 +13,22 @@ namespace dll.Metiers
         private string nom;
         private string prenom;
         private NiveauIds niveau;
+        private int classe;
 
         public int Id { get => id; set => id = value; }
         public string Nom { get => nom; set => nom = value; }
         public string Prenom { get => prenom; set => prenom = value; }
         public NiveauIds Niveau { get => niveau; set => niveau = value; }
+        public int Classe { get => classe; set => classe = value; }
 
         public static Utilisateur Load(int id)
         {
             return UtilisateurManager.Load(id);
+        }
+
+        public static List<Utilisateur> ListClasse(int classe)
+        {
+            return UtilisateurManager.ListClasse(classe);
         }
     }
 }
